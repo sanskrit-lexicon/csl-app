36170f70bcb5e6e5b4648117f0307c93ba0fb8ea
Dr. Dhaval Patel <drdhaval2785@gmail.com>
2026-04-09 16:07:53 +0530

    PWA on Google Chrome fix

--- lib/db_init_web.dart
+++ lib/db_init_web.dart
@@ -4,11 +4,21 @@
 // Note: databaseFactory (the global setter) is exported by sqflite_common_ffi.
 // databaseFactoryFfiWeb (the web implementation) is from sqflite_common_ffi_web.
 import 'package:sqflite_common_ffi/sqflite_ffi.dart' show databaseFactory;
-import 'package:sqflite_common_ffi_web/sqflite_ffi_web.dart'
-    show databaseFactoryFfiWeb;
+import 'package:sqflite_common_ffi_web/sqflite_ffi_web.dart';
 
 Future<void> initDatabaseFactory() async {
   // Assign the web (IndexedDB/WASM) factory to the global databaseFactory.
-  // This must be called before any database is opened.
-  databaseFactory = databaseFactoryFfiWeb;
+  // We use createDatabaseFactoryFfiWeb to enforce BasicWebWorker.
+  // 
+  // BUGFIX: Google Chrome supports SharedWorkers, but frequently fails to 
+  // load sqlite3.wasm or access IndexedDB correctly within them (especially as a PWA).
+  // Safari and Firefox intrinsically lack SharedWorker support so they fallback to
+  // BasicWorkers and succeed. This explicitly forces Chrome to do the same!
+  databaseFactory = createDatabaseFactoryFfiWeb(
+    options: SqfliteFfiWebOptions(
+      sharedWorkerUri: Uri.parse('sqflite_sw.js'),
+      // ignore: invalid_use_of_visible_for_testing_member
+      forceAsBasicWorker: true,
+    ),
+  );
 }
