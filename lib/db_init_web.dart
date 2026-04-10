cd9e872b44816035f012e3bf8034782c68d853be
Dr. Dhaval Patel <drdhaval2785@gmail.com>
2026-04-10 13:42:39 +0530

    sqlite3.wasm is necessary for web to work even though we do not use it

--- lib/db_init_web.dart
+++ lib/db_init_web.dart
@@ -5,13 +5,14 @@ import 'package:sqflite_common_ffi_web/sqflite_ffi_web.dart';
 Future<void> initDatabaseFactory() async {
   debugPrint('DB_INIT: Initializing sqflite_common_ffi_web factory...');
   try {
-    databaseFactory = createDatabaseFactoryFfiWeb(
-      options: SqfliteFfiWebOptions(
-        sharedWorkerUri: Uri.parse('sqflite_sw.js'),
-        // ignore: invalid_use_of_visible_for_testing_member
-        forceAsBasicWorker: true,
-      ),
+    final options = SqfliteFfiWebOptions(
+      sharedWorkerUri: Uri.parse('sqflite_sw.js'),
+      sqlite3WasmUri: Uri.parse('sqlite3.wasm'),
+      // ignore: invalid_use_of_visible_for_testing_member
+      forceAsBasicWorker: true,
     );
+    final factory = createDatabaseFactoryFfiWeb(options: options);
+    databaseFactory = factory;
     debugPrint('DB_INIT: databaseFactory successfully assigned.');
   } catch (e, stack) {
     debugPrint(
